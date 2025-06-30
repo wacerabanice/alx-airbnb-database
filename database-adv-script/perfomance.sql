a5a195fa7e0b92f2075316a761d59b9b1354c16e
Winnie Banice <93024882+wacerabanice@users.noreply.github.com>
2025-06-30 09:22:42 +0300

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,40 @@
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    p.property_id,
+    p.name AS property_name,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_method,
+    pay.payment_date
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+
+---
+
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    p.property_id,
+    p.name AS property_name,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_method,
+    pay.payment_date
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
